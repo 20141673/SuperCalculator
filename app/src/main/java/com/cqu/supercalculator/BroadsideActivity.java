27e3c7361d6e90aeff5b36c10b68f797b9333302
JerryYu1022 <412300460@qq.com>
2017-06-19 17:33:10 +0800

    实现标签页效果

--- app/src/main/java/com/cqu/supercalculator/BroadsideActivity.java
+++ app/src/main/java/com/cqu/supercalculator/BroadsideActivity.java
@@ -1,10 +1,10 @@
 package com.cqu.supercalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
 
 public class BroadsideActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +42,14 @@ public class BroadsideActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /*************Tab标签****************/
+        TabHost tabHost=(TabHost)findViewById(R.id.tabhost);
+        tabHost.setup();
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("简单",null).setContent(R.id.simplelayout));
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("科学",null).setContent(R.id.sciencelayout));
+
+
     }
 
     @Override
@@ -80,13 +90,10 @@ public class BroadsideActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_simple) {
+        if (id == R.id.nav_exchagerate) {
             // Handle the camera action
-
-        } else if (id == R.id.nav_science) {
-
-        } else if (id == R.id.nav_exchagerate) {
-
+            Intent intent=new Intent(BroadsideActivity.this,ExchageRateActivity.class);
+            BroadsideActivity.this.startActivity(intent);
         } else if (id == R.id.nav_houseloan) {
 
         } else if (id == R.id.nav_personincometax) {
