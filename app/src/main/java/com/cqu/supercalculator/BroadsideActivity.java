460d69198332ea9659b72b4735ccaa10112bb35c
JerryYu1022 <412300460@qq.com>
2017-06-19 11:18:47 +0800

    change toolbar

--- app/src/main/java/com/cqu/supercalculator/BroadsideActivity.java
+++ app/src/main/java/com/cqu/supercalculator/BroadsideActivity.java
@@ -80,17 +80,18 @@ public class BroadsideActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_simple) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            
+        } else if (id == R.id.nav_science) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_exchagerate) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_houseloan) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_personincometax) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_relativescall) {
 
         }
 
