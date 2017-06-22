68f806052709e36eecb1868d447a06c2ed4711e7
JerryYugo <412300460@qq.com>
2017-06-22 11:14:27 +0800

    长度换算界面初步完成

--- app/src/main/java/com/cqu/supercalculator/LengthExchangeActivity.java
+++ app/src/main/java/com/cqu/supercalculator/LengthExchangeActivity.java
@@ -9,5 +9,10 @@ public class LengthExchangeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_length_exchange);
+        initSpinner();
+    }
+
+    private void initSpinner(){
+
     }
 }
