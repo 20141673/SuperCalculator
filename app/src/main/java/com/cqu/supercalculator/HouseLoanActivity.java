75c95d54c21696c5a65b7c52e948c23f4192c30d
JerryYugo <412300460@qq.com>
2017-06-29 15:58:45 +0800

    房贷计算器的实现1.0

--- app/src/main/java/com/cqu/supercalculator/HouseLoanActivity.java
+++ app/src/main/java/com/cqu/supercalculator/HouseLoanActivity.java
@@ -1,24 +1,194 @@
 package com.cqu.supercalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TabHost;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class HouseLoanActivity extends AppCompatActivity {
+    private EditText etcommercialamount,etcommmercialpercent,etcommercialpercenttime;
+    private TextView tvcommercialpercent;
+
+    private EditText etprofundamount,etprofundpercent,etprofundpercenttime;
+    private TextView tvprofunpercent;
+
+    private Spinner houseloanrepayway,houseloanyearcount,houseloanyear,houseloanmonth,houseloanday;
+    private ArrayAdapter repaywayadapter,yearcountadapter;
+    private ArrayAdapter<String> yearadapter,monthadapter,dayadapter;
+    private int repayway,yearcount,year,month,day;
+    private double commercialamount=0,        //
+                   profundamount=0,
+                   commercialpercent=4.9,
+                   commercialpertime=1,
+                   profundpercent=3.25,
+                   profundpertime=1;
+    private double result=0;
+    private ArrayList<String> dataYear = new ArrayList<String>();
+    private ArrayList<String> dataMonth = new ArrayList<String>();
+    private ArrayList<String> dataDay = new ArrayList<String>();
+    private ArrayList<String> countYear=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_loan);
+        initView();
+        initSpinner();//初始化spinner
+    }
 
-        /*****初始化tabhost******/
-        TabHost tabHost=(TabHost)findViewById(R.id.houseloantabhost);
-        tabHost.setup();
-        //增加商业贷款的Tab
-        tabHost.addTab(tabHost.newTabSpec("commercial").setIndicator("商业贷款",null).setContent(R.id.commerciallayout));
-        //增加公积金贷款的Tab
-        tabHost.addTab(tabHost.newTabSpec("providentfund").setIndicator("公积金贷款",null).setContent(R.id.providentfundlayout));
-        //增加混合贷款的Tab
-        tabHost.addTab(tabHost.newTabSpec("mix").setIndicator("混合贷款",null).setContent(R.id.mixlayout));
+    private void initView(){
+        //商业贷款初始化
+        etcommercialamount=(EditText)findViewById(R.id.etcommercialamount);
+        etcommmercialpercent=(EditText)findViewById(R.id.etcommercialpercent);
+        etcommercialpercenttime=(EditText)findViewById(R.id.etcommercialpercenttime);
+        tvcommercialpercent=(TextView)findViewById(R.id.tvcommercialpercent);
+
+        //公积金贷款的初始化
+        etprofundamount=(EditText)findViewById(R.id.etprofundamount);
+        etprofundpercent=(EditText)findViewById(R.id.etprofundpercent);
+        etprofundpercenttime=(EditText)findViewById(R.id.etprofundpercenttime);
+        tvprofunpercent=(TextView)findViewById(R.id.tvprofundpercent);
+
+    }
+
+    private void initSpinner(){
+        //可重用的spinner组件初始化并设置adapter
+        houseloanrepayway=(Spinner)findViewById(R.id.houseloanrepayway);
+        houseloanyearcount=(Spinner)findViewById(R.id.houseloanyearcount);
+        houseloanday=(Spinner)findViewById(R.id.houseloanday);
+        houseloanmonth=(Spinner)findViewById(R.id.houseloanmonth);
+        houseloanyear=(Spinner)findViewById(R.id.houseloanyear);
+
+        repaywayadapter=ArrayAdapter.createFromResource(this,R.array.HouseRepayWay,android.R.layout.simple_spinner_item);
+        houseloanrepayway.setAdapter(repaywayadapter);
+        houseloanrepayway.setSelection(0);
+        for(int i=1;i<21;i++){
+            countYear.add(Integer.toString(i));
+        }
+        yearcountadapter=new ArrayAdapter(this,android.R.layout.simple_spinner_item,countYear);
+        houseloanyearcount.setAdapter(yearcountadapter);
+
+        houseloanrepayway.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> arg0,View arg1,int arg2,long arg3){
+               repayway=arg2;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0){}
+        });
+
+        houseloanyearcount.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> arg0,View arg1,int arg2,long arg3){
+                yearcount=arg2+1;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0){}
+        });
+
+        Calendar cal = Calendar.getInstance();
+        for (int i = 0; i < 30; i++) {
+            dataYear.add("" + (cal.get(Calendar.YEAR) - 15 + i));
+        }
+        yearadapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,dataYear);
+        yearadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        houseloanyear.setAdapter(yearadapter);
+        houseloanyear.setSelection(0);
+
+        for (int i = 1; i <= 12; i++) {
+            dataMonth.add("" + (i < 10 ? "0" + i : i));
+        }
+        monthadapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,dataMonth);
+        monthadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        houseloanmonth.setAdapter(monthadapter);
+
+        dayadapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,dataDay);
+        dayadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        houseloanday.setAdapter(dayadapter);
+
+        houseloanyear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> arg0,View arg1,int arg2,long arg3){
+                year=2002+arg2;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {}
+        });
+
+        houseloanmonth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                dataDay.clear();
+                Calendar cal = Calendar.getInstance();
+                cal.set(Calendar.YEAR, Integer.valueOf(houseloanyear.getSelectedItem().toString()));
+                cal.set(Calendar.MONTH, arg2);
+                int dayofm = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+                for (int i = 1; i <= dayofm; i++) {
+                    dataDay.add("" + (i < 10 ? "0" + i : i));
+                }
+                dayadapter.notifyDataSetChanged();
+                month=arg2+1;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {}
+        });
+
+        houseloanday.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
+                day=i+1;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0){}
+        });
+
+    }
+
+    public void clickButton(View v){
+        countData();
+        Intent intent=new Intent(HouseLoanActivity.this,HouseLoanResultActivity.class);
+        Bundle bundle=new Bundle();
+        bundle.putDouble("ComAmount",commercialamount);
+        bundle.putDouble("ProAmount",profundamount);
+        bundle.putInt("RePayWay",repayway);
+        bundle.putInt("YearCount",yearcount);
+        bundle.putInt("Year",year);
+        bundle.putInt("Month",month);
+        bundle.putInt("Day",day);
+        bundle.putDouble("ComPercent",commercialpercent*commercialpertime);
+        bundle.putDouble("ProPercent",profundpercent*profundpertime);
+        bundle.putDouble("YearCount",yearcount);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    private void countData(){
+        /*获得计算必要的数据项*/
+        if(!etcommercialamount.getText().toString().equals("")){
+            commercialamount=Double.parseDouble(etcommercialamount.getText().toString());
+        }
+        if(!etprofundamount.getText().toString().equals("")){
+            profundamount=Double.parseDouble(etprofundamount.getText().toString());
+        }
+        if(!etcommmercialpercent.getText().toString().equals("")){
+            commercialpercent=Double.parseDouble(etcommmercialpercent.getText().toString());
+        }
+        if(!etcommercialpercenttime.getText().toString().equals("")){
+            commercialpertime=Double.parseDouble(etcommmercialpercent.getText().toString());
+        }
+        if(!etprofundpercent.getText().toString().equals("")){
+            profundpercent=Double.parseDouble(etprofundpercent.getText().toString());
+        }
+        if(!etprofundpercenttime.getText().toString().equals("")){
+            profundpertime=Double.parseDouble(etprofundpercenttime.getText().toString());
+        }
     }
 }
--- app/src/main/java/com/cqu/supercalculator/HouseLoanResultActivity.java
+++ app/src/main/java/com/cqu/supercalculator/HouseLoanResultActivity.java
@@ -1,13 +1,79 @@
 package com.cqu.supercalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class HouseLoanResultActivity extends AppCompatActivity {
-
+    private TextView houseloanresult,tvinterestpay,tvtotalamount,tvyearamount,tvloanamount,tvmonthloss;
+    private TextView textView;
+    private LinearLayout repaylayout;
+    private double comamount,proamount,compercent,propercent,yearcount;
+    private int repayway,year,month,day;
+    private double result,total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_loan_result);
+        houseloanresult=(TextView)findViewById(R.id.houseloanresult);
+        tvinterestpay=(TextView)findViewById(R.id.tvinterestpay);
+        tvyearamount=(TextView)findViewById(R.id.tvyearamount);
+        tvloanamount=(TextView)findViewById(R.id.tvloanamount);
+        tvtotalamount=(TextView)findViewById(R.id.tvtotalamount);
+        textView=(TextView)findViewById(R.id.textView);
+        repaylayout=(LinearLayout)findViewById(R.id.linearLayoutrepay);
+        tvmonthloss=(TextView)findViewById(R.id.tvmonthloss);
+
+        /*获得传递的数据*/
+        Bundle bundle = this.getIntent().getExtras();
+        comamount=bundle.getDouble("ComAmount");
+        comamount*=10000;
+        proamount=bundle.getDouble("ProAmount");
+        proamount*=10000;
+        compercent=bundle.getDouble("ComPercent");
+        compercent=compercent/100/12;
+        propercent=bundle.getDouble("ProPercent");
+        propercent=propercent/100/12;
+        yearcount=bundle.getDouble("YearCount");
+        repayway=bundle.getInt("RePayWay");
+        year=bundle.getInt("Year");
+        month=bundle.getInt("Month");
+        day=bundle.getInt("Day");
+        countResult();
+
     }
+
+    private void countResult(){
+        java.text.DecimalFormat   df   =new   java.text.DecimalFormat("#.000");
+        if(repayway==0){  //等额本息方式
+            textView.setText("每月还款(元)");
+            double com=(comamount*compercent*Math.pow(1+compercent,yearcount*12))/(Math.pow(1+compercent,yearcount*12)-1);
+            double pro=(proamount*propercent*Math.pow(1+propercent,yearcount*12))/(Math.pow(1+propercent,yearcount*12)-1);
+            result=com+pro;
+            total=result*12;
+            houseloanresult.setText(String.valueOf(df.format(result)));                       //每月还款
+            tvinterestpay.setText(String.valueOf(df.format(total-comamount-proamount))+"元");
+            tvtotalamount.setText(String.valueOf(df.format(total/10000))+"万");
+            tvyearamount.setText(String.valueOf(yearcount)+"年");
+            tvloanamount.setText(String.valueOf(comamount+proamount)+"元");
+
+        }else if(repayway==1){  //等额本金方式
+            textView.setText("首月还款(元)");
+            repaylayout.setVisibility(View.VISIBLE);
+            double com=comamount/(yearcount*12)+comamount*compercent;
+            double pro=proamount/(yearcount*12)+proamount*propercent;
+            result=com+pro;
+            total=result*yearcount*12;
+            houseloanresult.setText(String.valueOf(df.format(result)));                       //每月还款
+            tvinterestpay.setText(String.valueOf(df.format(total-comamount-proamount))+"元");
+            tvtotalamount.setText(String.valueOf(df.format(total/10000))+"万");
+            tvyearamount.setText(String.valueOf(yearcount)+"年");
+            tvloanamount.setText(String.valueOf(comamount+proamount)+"元");
+            double monthloss=comamount/(yearcount*12)*(compercent/12)+proamount/(yearcount*12)*(compercent/12);
+            tvmonthloss.setText(String.valueOf(df.format(monthloss))+"元");
+        }
+    }
+
 }
