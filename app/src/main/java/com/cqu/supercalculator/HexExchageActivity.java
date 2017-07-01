22e9ea1577cb6cefb766f7acc8850b2845c01c25
JerryYugo <412300460@qq.com>
2017-07-01 16:23:22 +0800

    设计添加进制转换功能

---
+++ app/src/main/java/com/cqu/supercalculator/HexExchageActivity.java
@@ -0,0 +1,22 @@
+package com.cqu.supercalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class HexExchageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hex_exchage);
+    }
+
+    public void clicktagview(View v){
+
+    }
+
+    public void clicktextview(View v){
+
+    }
+}
